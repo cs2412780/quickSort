33a22449a13cc38a6adc929de097a78f33ac553e
Liang Dong <liangdong@cpp.edu>
2017-02-09 21:11:47 -0800

    QuickSort v1.4

--- QuickSort.java
+++ QuickSort.java
@@ -2,17 +2,19 @@ package quickSort;
 
 import java.util.Random;
 
+import homework2.LinkedDataStack;
 import lab1.ArrayBag;
+
 
 public class QuickSort {
 
 	public static void main(String[] args) {
-		ArrayBag<Integer> a1 = new ArrayBag<>(50000);
-		ArrayBag<Integer> a2 = new ArrayBag<>(50000);
+		ArrayBag<Integer> a1 = new ArrayBag<>(100000);
+		ArrayBag<Integer> a2 = new ArrayBag<>(100000);
 
 		Random r = new Random();
-		int[] array =new int[50000];
-		System.out.println("random integers: ");
+		int[] array =new int[100000];
+		
 		for(int i = 0; i < array.length; i++) {
 			array[i] = r.nextInt(500);
 			a1.add(array[i]);
@@ -22,25 +24,23 @@ public class QuickSort {
 		quickSortByIteration(array);
 		for(int i = 0; i < array.length; i++) {
 			a2.add(array[i]);
+			//System.out.println(array[i]);
 		}
-		for(int i = 0; i < array.length - 1; i++) {
-			if(array[i] > array[i + 1]) {
-				System.out.println("*****Failed");
-				break;
-			}
-		}
-		System.out.println(a1.equals(a2));
 		
+		
+
+		System.out.println("done\n");
+
 		a1.clear();
 		a2.clear();
-		
-		System.out.println("random integers: ");
 		for(int i = 0; i < array.length; i++) {
 			array[i] = r.nextInt(500);
 			a1.add(array[i]);
 		}
-		System.out.println("mergeSortByRecursion: ");
+		System.out.println("quickSortByRecursion: ");
+		
 		quickSortByRecursion(array);
+
 		for(int i = 0; i < array.length; i++) {
 			a2.add(array[i]);
 		}
@@ -50,58 +50,61 @@ public class QuickSort {
 				break;
 			}
 		}
-		System.out.println(a1.equals(a2));
+		System.out.println("done");
 
 	}
 	
 	public static void quickSortByIteration(int[] array) {
 		
-		int indexOfPivot = partition(array, 0, array.length - 1);
-		sort (array, 0, indexOfPivot);
-		sort (array, indexOfPivot + 1, array.length - 1);
-		
+		LinkedDataStack<Integer> stack = new LinkedDataStack<>();
+		stack.push(-1);
+		stack.push(array.length - 1);
+		stack.push(0);
+		int left;
+		int right;
+		int leftPointer;
+		int rightPointer;
+		int pivot;
+		while(stack.peek() != -1) {
+			left = stack.pop();
+			leftPointer =left;
+			right = stack.pop();
+			rightPointer = right;
+			pivot = array[(leftPointer + rightPointer) / 2];
+			while(rightPointer >= leftPointer) {
+				while(array[leftPointer] < pivot) {
+					leftPointer++;
+				}
+				while(array[rightPointer] > pivot) {
+					rightPointer--;
+				}
+				
+				if(rightPointer >= leftPointer) {
+					int temp = array[leftPointer];
+					array[leftPointer] = array[rightPointer];
+					array[rightPointer] = temp;
+					leftPointer++;
+					rightPointer--;
+				}
+			
+			}
+			if(rightPointer > left) {
+				stack.push(rightPointer);
+				stack.push(left);
+			}
+			
+			if(right > leftPointer) {
+				stack.push(right);
+				stack.push(leftPointer);
+			}
+		}
 	}
 	
-	private static void sort (int[] array, int first, int last) {
-		for (int i = first; i <= last; i++) {
-			int j = i;
-			while((j > first) && (array[j] < array[j-1])) {
-				int temp = array[j];
-				array[j] = array[j-1];
-				array[j-1] = temp;
-				j--;
-			}//end while
-		}//end for
-	}//end sort
-	
-	
-	private static int partition(int[] array, int firstIndex, int lastIndex) {
-		int left = firstIndex;
-		int right = lastIndex;
-		int pivot = array[(firstIndex + lastIndex) / 2];
-		while(left <= right) {
-			while(array[left] < pivot) {
-				left++;
-			}
-			while(array[right] > pivot) {
-				right--;
-
-			}
-			if(left <= right) {
-				int temp = array[left];
-				array[left] = array[right];
-				array[right] = temp;
-				left++;
-				right--;
-			}
-		}//end while
-		return right;
-	}
 	
 	public static void quickSortByRecursion(int[] array) {
 
 		doQuickSort(array, 0, array.length - 1);
-		}
+	}
 
 		private static void doQuickSort(int[] array, int firstIndex, int lastIndex) {
 			int left = firstIndex;
